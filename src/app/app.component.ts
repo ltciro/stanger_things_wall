2c7c5dc20bcc96128f985d1e485ed39697266193
ltciro <ltciro@gmail.com>
2017-10-10 23:35:12 -0500

    wall messages keypress and click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
+import { trigger, state, style, animate, transition, keyframes, query, stagger } from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  letters: [string[]] = [
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+    [ 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q'],
+    ['r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']];
+  letterActive: string;
+
+  pressLetter(letter: string): void {
+    this.changeLetter(letter);
+  }
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    this.changeLetter(event.key);
+  }
+  changeLetter(letter): void {
+    if (letter === this.letterActive) {
+      this.letterActive = '';
+      setTimeout(() => {
+        this.letterActive = letter;
+      }, 100);
+      return;
+    }
+    // why this here
+    this.letterActive = letter;
+  }
 }
